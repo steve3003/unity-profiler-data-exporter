050199edd62381d82a5d9599871cb0e4168a5199
Stefano Di Palma <steve3003@hotmail.it>
2018-08-27 12:35:18 +0100

    Removed garbage from newest Unity versions null ref fix in GUIClip wrapper

--- UnityWrappers/GUIClip.cs
+++ UnityWrappers/GUIClip.cs
@@ -4,29 +4,35 @@ using UnityEngine;
 
 namespace ProfilerDataExporter
 {
-    /// <summary>
-    /// Wrapper for unity internal GUIClip class
-    /// </summary>
-    public class GUIClip
-    {
-        public static Rect visibleRect
-        {
-            get
-            {
-                Func<Rect> VisibleRect = null; 
-                var tyGUIClip = Type.GetType("UnityEngine.GUIClip,UnityEngine");
-                if (tyGUIClip != null)
-                {
-                    var piVisibleRect = tyGUIClip.GetProperty("visibleRect", BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
-                    if (piVisibleRect != null)
-                    {
-                        var getMethod = piVisibleRect.GetGetMethod(true) ?? piVisibleRect.GetGetMethod(false);
-                        VisibleRect = (Func<Rect>)Delegate.CreateDelegate(typeof(Func<Rect>), getMethod);
-                    }
-                }
+	/// <summary>
+	/// Wrapper for unity internal GUIClip class
+	/// </summary>
+	public class GUIClip
+	{
+		private static readonly Func<Rect> VisibleRectDelegate;
 
-                return VisibleRect();
-            }
-        }
-    }
+		static GUIClip()
+		{
+			var guiClipType = Type.GetType("UnityEngine.GUIClip,UnityEngine");
+
+			var visibleRectMethodInfo = guiClipType.GetProperty(
+				"visibleRect",
+				BindingFlags.DeclaredOnly |
+				BindingFlags.Static |
+				BindingFlags.Public |
+				BindingFlags.NonPublic |
+				BindingFlags.GetProperty)
+				.GetGetMethod(true);
+
+			VisibleRectDelegate = (Func<Rect>)Delegate.CreateDelegate(typeof(Func<Rect>), visibleRectMethodInfo);
+		}
+
+		public static Rect visibleRect
+		{
+			get
+			{
+				return VisibleRectDelegate();
+			}
+		}
+	}
 }
